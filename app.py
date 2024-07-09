3d0e7775faed1bef03dd25d6568979885d22f0f1
Matteo <matteo.langone@enel.com>
2024-07-09 18:25:22 +0200

    templates and static folder + completing base code

--- app.py
+++ app.py
@@ -1,29 +1,25 @@
 import re
 from datetime import datetime
 
-from flask import Flask
+
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def home():
-    return "Hello, Flask!"
+def home(): return render_template("home.html")
 
+@app.route("/about")
+def about(): return render_template("about.html")
 
+@app.route("/contact")
+def contact(): return render_template("contact.html")
+
+@app.route("/hello")
 @app.route("/hello/<name>") # The identifier inside < and > in the route defines a variable that is passed to the function and can be used in your code.
-def hello_there(name):
-    now = datetime.now()
-    formatted_now = now.strftime("%A, %d %B, %Y at %X")
+def hello_there(name = "Matteo"): return render_template( "hello_there.html", name=name, date=datetime.now() )   
 
-    # Filter the name argument to letters only using regular expressions. URL arguments
-    # can contain arbitrary text, so we restrict to safe characters only.
-    match_object = re.match("[a-zA-Z]+", name)
-
-    if match_object:
-        clean_name = match_object.group(0)
-    else:
-        clean_name = "Friend"
-
-    content = "Hello there, " + clean_name + "! It's " + formatted_now
-    return content
+@app.route("/api/data") 
+def get_data():
+    return app.send_static_file("data.json")
